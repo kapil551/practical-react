3e8c5822e6b513ec063da15ca2bd5ef4190b4e91
Kapil Chaudhary <chaudharikapil16@gmail.com>
2022-01-29 07:41:38 +0530

    1 - Icons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { FaReact } from "react-icons/fa";
+
+import { MdAccountBalance, MdAlarm } from "react-icons/md";
+
+import { IconContext } from "react-icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+
+      <div className="App">
+        <FaReact />
+        <FaReact color="purple" />
+        <FaReact size="10rem" color="blue" />
+
+        <MdAlarm />
+        <MdAlarm color="red" size="15rem" />
+      </div>
+
+      {/* Adding common styling to multiple icons using IconContext.Provider component */}
+      <IconContext.Provider value={{ color: 'green', size: '5rem' }}>
+        <div className="App">
+          <FaReact />
+          <FaReact />
+          <FaReact />
+
+          <MdAlarm />
+          <MdAccountBalance />
+
+          {/* Inline styling has higher precedence */}
+          <MdAccountBalance color="brown" size="10rem" />
+
+        </div>
+      </IconContext.Provider>
+
+    </>
   );
 }
 
